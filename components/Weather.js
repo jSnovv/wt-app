5d9f7e503f10af1c133aeed5806874e14b849156
Amnee Hima <amneehima@gmail.com>
2022-08-25 22:37:19 +0700

    Create Forecast.js

---
+++ components/Weather.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import Forecast from './Forecast';
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    }) 
+    return (
+        <Forecast {...forecastInfo}/>
+    );
+}         
