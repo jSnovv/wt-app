49a17fe1331c06fe74a897dd18e71681964fd078
Amnee Hima <amneehima@gmail.com>
2022-08-25 22:57:17 +0700

    Add bg.jpg

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { ImageBackground, StyleSheet, Text } from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
@@ -9,6 +9,19 @@ export default function Weather(props) {
         temp: 0
     }) 
     return (
-        <Forecast {...forecastInfo}/>
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+          <Text>Zip Code</Text>
+          <Text>{props.zipCode}</Text>
+          <Forecast {...forecastInfo}/>  
+        </ImageBackground>
     );
-}         
+}     
+
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+});
+   
