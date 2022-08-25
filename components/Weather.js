b57350b37fff1b08419fb98b5ba5dc6a1989c8f7
Amnee Hima <amneehima@gmail.com>
2022-08-26 06:56:05 +0700

    section4

--- components/Weather.js
+++ components/Weather.js
@@ -19,6 +19,7 @@ export default function Weather(props) {
 
 const styles = StyleSheet.create({
     backdrop: {
+        justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
         height: '100%'
